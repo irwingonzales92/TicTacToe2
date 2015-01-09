b2615aa082d17a2b3f3b137ebfb900a63763a2df
irwingonzales92 <irwin@newdesto.com>
2015-01-08 19:07:13 -0800

    Initial Commit

---
+++ TicTacToe2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe2
+//
+//  Created by Irwin Gonzales on 1/8/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
